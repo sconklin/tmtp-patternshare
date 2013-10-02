81143bbbf46e12b663422c101d2a8312943c959e
Susan Spencer <susan.spencer@gmail.com>
2013-10-02 13:12:31 -0500

    Changed file to call google-chrome

--- startTmtp.sh
+++ startTmtp.sh
@@ -1,3 +1,3 @@
 #python -m SimpleHTTPServer 
-chromium-browser --allow-file-access-from-files http://localhost:8000/index.html
+google-chrome --allow-file-access-from-files http://localhost:8000/index.html
 #firefox http://localhost:8000/index.html
